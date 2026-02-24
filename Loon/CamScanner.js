c049636409bab3f82f69d9cb590eb8949ec64794
GLH08 <0688266@gmail.com>
2026-02-24 21:06:51 +0800

    Update CamScanner.js

--- Loon/CamScanner.js
+++ Loon/CamScanner.js
@@ -1,12 +1,3 @@
-/*
- ** CamScanner 解锁部分高级特权
-*/
-
 let obj = JSON.parse($response.body);
-$done({body: JSON.stringify({
-  "data": {
-    "psnl_vip_property": {
-      "expiry": "2013017600"
-    }
-  }
-})});
+obj = {"data":{"psnl_vip_property":{"expiry":4102415999,"svip": 1,"nxt_renew_tm":4102415999}}};
+$done({body: JSON.stringify(obj)});
